230b60819e454919681ef042f3cff956b8f3a261
aarnav singh <aarnavsingh836@gmail.com>
2023-10-24 11:16:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 from games.coin_flip import coin_flip
 from games.dice_roll import dice_roll
 from games.guess_random_number import guess_random_number
+from games.mathStudyBuddy import mathStudyBuddy
 
 def main_menu():
     while True:
