42af8f2293bf2daa7f69d7197117c2108aa30d70
aarnav singh <aarnavsingh836@gmail.com>
2023-10-24 18:34:26 -0500

    Add function definition for True_or_False.py

--- games/True_or_False.py
+++ games/True_or_False.py
@@ -1,25 +1,26 @@
-questions = [
-    "Is there class this thursday?",
-    "Will it be in the 30s next week?",
-    "Did I use any breaks in this code?",
-    "Are you passing this class?",
-    "Is the sky blue?",
-]
-
-answers = [False, True, False, True, True]
-
-for i, question in enumerate(questions):
-    user_answer = input(f"{question} (True/False): ").strip().lower()
+def torf():
+    questions = [
+        "Is there class this thursday?",
+        "Will it be in the 30s next week?",
+        "Did I use any breaks in this code?",
+        "Are you passing this class?",
+        "Is the sky blue?",
+    ]
     
-    if user_answer == 'true':
-        user_response = True
-    elif user_answer == 'false':
-        user_response = False
-    else:
-        print("Invalid input, please use 'True' or 'False'.")
-        continue
+    answers = [False, True, False, True, True]
     
-    if user_response == answers[i]:
-        print("Correct!")
-    else:
-        print("Incorrect!")
+    for i, question in enumerate(questions):
+        user_answer = input(f"{question} (True/False): ").strip().lower()
+        
+        if user_answer == 'true':
+            user_response = True
+        elif user_answer == 'false':
+            user_response = False
+        else:
+            print("Invalid input, please use 'True' or 'False'.")
+            continue
+        
+        if user_response == answers[i]:
+            print("Correct!")
+        else:
+            print("Incorrect!")
