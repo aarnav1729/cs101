4ce43d3a84bb6ba1f8f0add87029db3772d62fd9
asialee22 <148891304+asialee22@users.noreply.github.com>
2023-11-15 00:26:31 -0600

    game code

--- main.py
+++ main.py
@@ -1,70 +1,27 @@
+
+
+# Guessing the number game
+print("Guess what number 1-20 the computer is thinking of in 2 tries")
+
+
 import random
-from games.coin_flip import coin_flip
-from games.guess_random_number import guess_random_number
-from games.mathStudyBuddy import mathStudyBuddy
-from games.True_or_False import torf
-from games.three_dice import three_dice
-from games.heath_game import heath_game
-from games.make_cake import make_cake
-from games.guess_letter import guess_letter
-from games.blackjack import blackjack
-from games.RockPaperScissors import RockPaperScissors
+number = random.randint(1, 20)  
 
-def main_menu():
-    while True:
 
-        print("main menu:")
-        print("1. guess the random number- aarnav")
-        print("2. math study buddy- wyatt")
-        print("3. coin flip- aarnav")
-        print("4. blackjack- elisha")
-        print("5. true or false- jonathan")
-        print("6. three dice game- anna")
-        print("7. make your own cake- leon")
-        print("8. heath game- layla")
-        print("9. guess the letter- cornelius")
-        print("10. rock paper scissors- marcus")
-        print("0. exit")
+if number % 2 == 0:
+    print("The number is Even")
+else:
+    print("The number is Odd")
 
-        choice = input("which game do you want to play: ")
 
-        if choice == "1":
-            guess_random_number()
+for i in range(2):  
+    guess = int(input("Enter your guess: "))
+    if guess == number:
+        print("You win! You're a genius")
+        break
+    else:
+        print("You are incorrect!")
 
-        elif choice == "2":
-            mathStudyBuddy()
 
-        elif choice == "3":
-            coin_flip()
-
-        elif choice == "4":
-            blackjack()
-
-        elif choice == "5":
-            torf()
-
-        elif choice == "6":
-            three_dice()
-
-        elif choice == "7":
-            heath_game()
-
-        elif choice == "8":
-            make_cake()
-
-        elif choice == "9":
-            guess_letter()
-
-        elif choice == "10":
-            RockPaperScissors()
-
-        elif choice == "0":
-            print("goodbye!")
-
-            break
-
-        else:
-            print("invalid selection, please try again")
-
-if __name__ == "__main__":
-    main_menu()
+if guess != number:
+    print("Unfortunately, you are out of turns. Try Again")
